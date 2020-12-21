802bcf3bec7a77741bd5e49b15a10bdbe90502e4
Arelir <1825404269@qq.com>
2020-12-21 09:46:48 +0800

    完成了主页功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,14 +13,25 @@ $(function() {
 
     //从layui中获取form 对象
     var form = layui.form   //与$ 相似
-    // var layer = layer.layer
+    var layer = layui.layer
 
     //通过 form.verify() 函数自定义校验规则
 
      form.verify({
          //自定义一个叫 pwd 的 校验规则
-         pwd: [/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格']
-     })
+         pwd: [/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'],
+         // 校验两次密码是否一致的规则
+         repwd: function(value) {
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败,则return一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+            return '两次密码不一致！'
+            }  
+        }
+    })
 
 
 
@@ -45,12 +56,12 @@ $(function() {
      })
 
      //监听登录表单的提交事件
-     $('#from_login').on('submit', function(e){
+     $('#from_login').submit(function(e){
          //阻止默认提交行为
         e.preventDefault();
         $.ajax({
             url: '/api/login',
-            method: "POST",
+            method: 'POST',
             //快速获取表单数据
             data: $(this).serialize(),
             success: function(res) {
